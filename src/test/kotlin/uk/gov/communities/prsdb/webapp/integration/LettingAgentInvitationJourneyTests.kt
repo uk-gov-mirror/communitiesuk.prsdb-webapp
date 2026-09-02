a055a398de9995d033c6045ded6c14c74081497d
Tom Hanmer <tom.hanmer@softwire.com>
2026-09-02 11:35:17 +0100

    PDJB-1567: additional tests

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LettingAgentInvitationJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LettingAgentInvitationJourneyTests.kt
@@ -1,8 +1,10 @@
 package uk.gov.communities.prsdb.webapp.integration
 
 import com.microsoft.playwright.Page
+import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.communities.prsdb.webapp.constants.DELEGATE_TO_LETTING_AGENT
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.BaseComponent
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage.Companion.assertPageIs
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.lettingAgentInvitationJourneyPages.EnterPasswordPage
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.lettingAgentInvitationJourneyPages.PasswordCreationConfirmationPage
@@ -27,8 +29,14 @@ class LettingAgentInvitationJourneyTests : IntegrationTestWithMutableData("data-
         val setPasswordPage = assertPageIs(page, SetPasswordPage::class)
         setPasswordPage.form.submit()
 
-        // TODO PDJB-1567: Assert confirmation page content once the upstream stub steps provide real data
         val confirmationPage = assertPageIs(page, PasswordCreationConfirmationPage::class)
+        BaseComponent
+            .assertThat(confirmationPage.confirmationBanner)
+            .containsText("Property password created")
+        assertThat(confirmationPage.insetText).containsText("Check you have a copy of this link")
+        // TODO PDJB-1661: Update the expected update link once the real invitation link is wired in
+        assertThat(confirmationPage.updateLink.locator).hasAttribute("href", "https://example.com")
+        assertThat(confirmationPage.updateLink.locator).hasText("https://example.com")
         confirmationPage.form.submit()
 
         // TODO PDJB-1659: Remove this step from the journey test
--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/lettingAgentInvitationJourneyPages/PasswordCreationConfirmationPage.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/pageObjects/pages/lettingAgentInvitationJourneyPages/PasswordCreationConfirmationPage.kt
@@ -1,14 +1,17 @@
 package uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.lettingAgentInvitationJourneyPages
 
 import com.microsoft.playwright.Page
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.ConfirmationBanner
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Form
+import uk.gov.communities.prsdb.webapp.integration.pageObjects.components.Link
 import uk.gov.communities.prsdb.webapp.integration.pageObjects.pages.basePages.BasePage
 import uk.gov.communities.prsdb.webapp.journeys.lettingAgentInvitation.steps.ConfirmationStep
 
-// TODO PDJB-1567: Page object is intentionally minimal for now. Extend with content assertions (banner,
-//  update link, password row) once the upstream stub steps can drive real data onto this page.
 class PasswordCreationConfirmationPage(
     page: Page,
 ) : BasePage(page, "/${ConfirmationStep.ROUTE_SEGMENT}") {
+    val confirmationBanner = ConfirmationBanner(page)
+    val updateLink = Link(page.locator("#update-link"))
+    val insetText = page.locator(".govuk-inset-text")
     val form = Form(page)
 }
