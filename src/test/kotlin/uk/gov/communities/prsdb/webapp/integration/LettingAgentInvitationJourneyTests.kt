75f04d856706219ea4e23f7ce790f459567ea618
Tom Hanmer <tom.hanmer@softwire.com>
2026-09-02 13:57:46 +0100

    PDJB-1567: clean up test

--- src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LettingAgentInvitationJourneyTests.kt
+++ src/test/kotlin/uk/gov/communities/prsdb/webapp/integration/LettingAgentInvitationJourneyTests.kt
@@ -34,7 +34,6 @@ class LettingAgentInvitationJourneyTests : IntegrationTestWithMutableData("data-
             .assertThat(confirmationPage.confirmationBanner)
             .containsText("Property password created")
         assertThat(confirmationPage.backLink.locator).hasCount(0)
-        assertThat(confirmationPage.insetText).containsText("Check you have a copy of this link")
         // TODO PDJB-1661: Update the expected update link once the real invitation link is wired in
         assertThat(confirmationPage.updateLink.locator).hasAttribute("href", "https://example.com")
         assertThat(confirmationPage.updateLink.locator).hasText("https://example.com")
